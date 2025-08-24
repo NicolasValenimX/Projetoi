5f69541eb25f53a0bb68923601ea0a9a58c4c3a8
NicolasValentimX <nvalentim824@gmail.com>
2025-08-23 21:18:03 -0300

    Novo commit

--- NovoProjeto/Program.cs
+++ NovoProjeto/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Usuario user = new Usuario();
+
+        Console.Write("Digite seu nome: ");
+        user.Nome = Console.ReadLine();
+
+        // Idade com validação
+        int idade;
+        while (true)
+        {
+            Console.Write("Digite sua idade: ");
+            if (int.TryParse(Console.ReadLine(), out idade) && idade >= 0 && idade <= 120)
+            {
+                user.Idade = idade;
+                break;
+            }
+            Console.WriteLine("Idade inválida, tenta de novo.");
+        }
+
+        // Salário com validação
+        decimal salario;
+        while (true)
+        {
+            Console.Write("Digite seu salário: ");
+            if (decimal.TryParse(Console.ReadLine(), out salario) && salario >= 0)
+            {
+                user.Salario = salario;
+                break;
+            }
+            Console.WriteLine("Salário inválido, tenta de novo.");
+        }
+
+        Console.WriteLine($"\nNome: {user.Nome}, Idade: {user.Idade}, Salário: {user.Salario:C}");
+
+        if (user.Idade < 18)
+            Console.WriteLine("Vai estudar, moleque!");
+
+        if (user.Salario < 1500)
+            Console.WriteLine("Tá foda, precisa ganhar mais.");
+        else
+            Console.WriteLine("Dá pra sobreviver, mas não vacila.");
+    }
+}
+
+public class Usuario
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+    public decimal Salario { get; set; }
+}
